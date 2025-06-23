4cd7402114da26d2b894fac8fa05b811b944ee48
tanchaowen84 <tanchaowen84@gmail.com>
2025-06-23 20:42:20 +0800

    fix:delete waitlist and roadmaps route and pages for more simple web project

--- src/routes.ts
+++ src/routes.ts
@@ -14,7 +14,6 @@ export enum Routes {
   Docs = '/docs',
   About = '/about',
   Contact = '/contact',
-  Waitlist = '/waitlist',
   Changelog = '/changelog',
   Roadmap = 'https://mksaas.link/roadmap',
   CookiePolicy = '/cookie',
